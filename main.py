b5eccc2d38d19639736ab2edcc7a1be577ba1983
richard Tonnang <rtonnang@hotmail.de>
2025-12-16 18:10:52 +0100

    path0

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# Modèle pour la requête JSON
-class SentimentRequest(BaseModel):
-    text: str
-
-@app.get("/status")
-def read_status():
-    return {"status": "ok"}
+class User(BaseModel):
+    username: str
+    password: str
 
 @app.get("/permissions")
 def get_permissions(username: str, password: str):
@@ -17,16 +13,4 @@ def get_permissions(username: str, password: str):
         return {"permissions": "full access"}
     else:
         raise HTTPException(status_code=403, detail="Forbidden")
-
-@app.post("/v1/sentiment")
-def sentiment_v1(data: SentimentRequest):
-    text = data.text
-    sentiment_score = 1 if "love" in text.lower() else -1
-    return {"score": sentiment_score}
-
-@app.post("/v2/sentiment")
-def sentiment_v2(data: SentimentRequest):
-    text = data.text
-    sentiment_score = -1 if "hate" in text.lower() else 1
-    return {"score": sentiment_score}
 

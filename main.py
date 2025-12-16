d306d99ebdc0ef45f5fe31f84e332bb84e27dd12
richard Tonnang <rtonnang@hotmail.de>
2025-12-16 17:23:01 +0100

    check path

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 @app.get("/status")
 def status():
     return {"status": "ok"}
+
+
+
+@app.post("/v2/sentiment")
+def sentiment_v2(text: str):
+    if text == "I hate this!":
+        return {"sentiment": "negative"}
+    return {"sentiment": "positive"}
 

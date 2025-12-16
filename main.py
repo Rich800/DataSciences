892dd32d17a7a2992717b8ff5aa93207762194a6
richard Tonnang <rtonnang@hotmail.de>
2025-12-16 17:52:45 +0100

    path1

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Définir la route /status
 @app.get("/status")
 def read_status():
-    return {"status": "oK"}
+    return {"status": "ok"}
+
+@app.post("/v1/sentiment")
+def sentiment_v1(data: dict):
+    text = data.get("text")
+    # Ajoute ici ta logique d'analyse de sentiment
+    return {"sentiment": "positive" if "love" in text else "negative"}
 
